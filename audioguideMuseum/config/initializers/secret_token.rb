016cb9c56be60b41f8716e0a9a5ffd7529cfc279
Alex Stanese <alecs_theone@yahoo.com>
2013-07-01 13:59:03 +0300

    all folders in audioguide folder

--- audioguideMuseum/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-AudioguideMuseum::Application.config.secret_token = '411308434a45b0e012ef87a0a99173ddfc58f5ef381c361ed69fa37e38f5771ea6798af06ad064d95da76177d8d74b0254a0567c15dbde671223f39e67001065'
