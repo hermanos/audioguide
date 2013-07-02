3d60167aad1f5fcef8edd6af8d92e5c1bc3f2833
rares123 <cruceat.rares@yahoo.com>
2013-07-02 16:28:39 +0300

    Remove column coins

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,21 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130701111506) do
+ActiveRecord::Schema.define(:version => 20130702132730) do
+
+  create_table "museums", :force => true do |t|
+    t.string   "name"
+    t.integer  "lat"
+    t.integer  "long"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "username"
     t.string   "password"
     t.string   "email"
     t.integer  "role"
-    t.integer  "coins"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
