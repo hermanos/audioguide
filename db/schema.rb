0cee275675816b4c019576c8bbdb3b81ecf22de7
Alex Stanese <alecs_theone@yahoo.com>
2013-07-03 17:11:31 +0300

    background

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,23 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130701111506) do
+ActiveRecord::Schema.define(:version => 20130701111940) do
+
+  create_table "jokes", :force => true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "museums", :force => true do |t|
+    t.string   "name"
+    t.integer  "lat"
+    t.integer  "long"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "username"
