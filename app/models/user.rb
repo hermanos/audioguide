e6ec6eff583a4c298c5b7b91921213a40b8f5675
davidlinx <david_suilea@yahoo.com>
2013-07-08 13:00:19 +0300

    Work work

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :id, :email, :password, :password_confirmation, :remember_me, :profile
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :profile
   # attr_accessible :title, :body
   
 	has_one :profile
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,15 +3,21 @@
 #
 # Examples:
 #
-  admin = User.create!(email: 'admin@audioguide.com', password:'admin123')
-  admin.profile.update_attribute(:role, "admin")
-  manager = User.create(email: 'manager@audioguide.com', password: 'manager123')
-  manager.profile.update_attribute(:role, "manager")
-  user = User.create(email: 'user23434@audioguide.com', password: 'user123')
-  Museum.create([{name: 'Muzeul de Stiinte Naturale', lat: 45, long: 40 }, { name: 'Muzeul Brukenthal', lat: 45, long: 40 }])
-
-
   achievement1 = Achievement.create!(title: "First Scan", description: "Congratulations for your first Scan !", image: "no_image")
   achievement2 = Achievement.create!(title: "100 Scans", description: "Congratulations for your 100th Scan !", image: "no_image")
   achievement3 = Achievement.create!(title: "Full Museum", description: "Congratulations for visiting an entire Museum !", image: "no_image")
   achievement4 = Achievement.create!(title: "Rich", description: "Congratulations for optaining 1000 coins !", image: "no_image")
+
+  admin = User.create!(email: 'admin@audioguide.com', password:'admin123')
+  admin.profile.update_attribute(:role, "admin")
+  manager = User.create(email: 'manager@audioguide.com', password: 'manager123')
+  manager.profile.update_attribute(:role, "manager")
+  user = User.create(email: 'basic_user@audioguide.com', password: 'user1234')
+  user.profile.achievements << achievement1
+  user.profile.achievements << achievement2
+
+  Museum.create([{name: 'Muzeul de Stiinte Naturale', lat: 45, long: 40 }, { name: 'Muzeul Brukenthal', lat: 45, long: 40 }])
+
+
+  
+
