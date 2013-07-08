e8a945dd3e4a8f83caed43962eb436e6925ed3fb
octapopa <octapopa@yahoo.com>
2013-07-08 09:50:16 +0300

    change dashboars

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,8 @@ class PagesController < ApplicationController
   
   def index
     if user_signed_in?
-      redirect_to dashboard_path
+      @user = current_user
+      render dashboard
       return
     end
 
