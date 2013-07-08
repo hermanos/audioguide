7cc428c2ff4cad3948396124c74e2077762dbc91
rares123 <cruceat.rares@yahoo.com>
2013-07-08 11:57:11 +0300

    Change qr-code obj to qr-code url

--- app/models/exhibit.rb
+++ app/models/exhibit.rb
@@ -6,8 +6,8 @@ class Exhibit < ActiveRecord::Base
   after_create :create_qr_code
 
   def create_qr_code
-
-  	crypt_code = Digest::MD5.hexdigest(id.to_s + "-" + Time.now.to_s)
+  	exhibit_url = "http://localhost:3000/museums/" + museum_id.to_s + "/exhibits/" + id.to_s
+  	crypt_code = Digest::MD5.hexdigest(exhibit_url)
   	update_attribute(:qr_code, crypt_code)
 
   end
