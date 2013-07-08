c510b8f26a7791ce9f38fd5c20fa3d32e9bddb76
rares123 <cruceat.rares@yahoo.com>
2013-07-08 10:08:33 +0300

    Add gem rqrcode for qrcode

--- app/models/exhibit.rb
+++ app/models/exhibit.rb
@@ -2,4 +2,14 @@ class Exhibit < ActiveRecord::Base
   attr_accessible :audio, :description, :floor, :lat, :long, :museum_id, :qr_code, :title, :video
 
   belongs_to :museum
+
+  after_create :create_qr_code
+
+  	def create_qr_code
+
+  		qr_code = {id_exhibit: @exhibit.id, }
+
+  	end
+
+  end
 end
