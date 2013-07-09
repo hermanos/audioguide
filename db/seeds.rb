cd66df0cacaf949564a4a748e0b0bcad4580686c
rares123 <cruceat.rares@yahoo.com>
2013-07-09 17:18:38 +0300

    change publish to private

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@
 
   Museum.create([{name: 'Muzeul de Stiinte Naturale', lat: 45, long: 40 }, { name: 'Muzeul Brukenthal', lat: 45, long: 40 }])
 
-  exhibit1 = Exhibit.create({title: 'Sculptura transilvăneană în piatră din secolele XIII-XIX', description: 'Colecţia de piese sculpturale transilvănene medievale şi moderne timpurii din piatră a fost constituită treptat, începând cu prima jumătate a secolului al XIX-lea. ',audio: nil, video: nil, floor: 0, lat: 40, long: 40, qr_code: 'http://10.0.0.64:3000/museums/2/exhibits/1',museum_id: 2,publish: 0});
-  exhibit2 = Exhibit.create({title: 'Monalisa', description: 'Pictura. ',audio: nil, video: nil, floor: 0, lat: 40, long: 40, qr_code: 'http://10.0.0.64:3000/museums/2/exhibits/2',museum_id: 2,publish: 0});
+  exhibit1 = Exhibit.create({title: 'Sculptura transilvăneană în piatră din secolele XIII-XIX', description: 'Colecţia de piese sculpturale transilvănene medievale şi moderne timpurii din piatră a fost constituită treptat, începând cu prima jumătate a secolului al XIX-lea. ',audio: nil, video: nil, floor: 0, lat: 40, long: 40, qr_code: 'http://10.0.0.64:3000/museums/2/exhibits/1',museum_id: 2});
+  exhibit2 = Exhibit.create({title: 'Monalisa', description: 'Pictura. ',audio: nil, video: nil, floor: 0, lat: 40, long: 40, qr_code: 'http://10.0.0.64:3000/museums/2/exhibits/2',museum_id: 2});
   scan1 = Scan.create(profile_id: user.profile.id, exhibit_id: exhibit1.id)
   scan2 = Scan.create(profile_id: user.profile.id, exhibit_id: exhibit2.id)
