e87c1d9ef809959dc020fa4f35cf24c89853aace
rares123 <cruceat.rares@yahoo.com>
2013-07-09 16:38:46 +0300

    Add scan seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,9 +13,13 @@
   manager = User.create(email: 'manager@audioguide.com', password: 'manager123')
   manager.profile.update_attribute(:role, "manager")
   user = User.create(email: 'basic_user@audioguide.com', password: 'user1234')
+  user.profile.update_attribute(:coins, 100)
   user.profile.achievements << achievement1
   user.profile.achievements << achievement2
 
   Museum.create([{name: 'Muzeul de Stiinte Naturale', lat: 45, long: 40 }, { name: 'Muzeul Brukenthal', lat: 45, long: 40 }])
 
-  Exhibit.create([{title: 'Sculptura transilvăneană în piatră din secolele XIII-XIX', description: 'Colecţia de piese sculpturale transilvănene medievale şi moderne timpurii din piatră a fost constituită treptat, începând cu prima jumătate a secolului al XIX-lea. ',audio: nil, video: nil, floor: 0, lat: 40, long: 40, qr_code: 'http://10.0.0.64:3000/museums/2/exhibits/1',museum_id: 2,publish: 0}]);
+  exhibit1 = Exhibit.create({title: 'Sculptura transilvăneană în piatră din secolele XIII-XIX', description: 'Colecţia de piese sculpturale transilvănene medievale şi moderne timpurii din piatră a fost constituită treptat, începând cu prima jumătate a secolului al XIX-lea. ',audio: nil, video: nil, floor: 0, lat: 40, long: 40, qr_code: 'http://10.0.0.64:3000/museums/2/exhibits/1',museum_id: 2,publish: 0});
+  exhibit2 = Exhibit.create({title: 'Monalisa', description: 'Pictura. ',audio: nil, video: nil, floor: 0, lat: 40, long: 40, qr_code: 'http://10.0.0.64:3000/museums/2/exhibits/2',museum_id: 2,publish: 0});
+  scan1 = Scan.create(profile_id: user.profile.id, exhibit_id: exhibit1.id)
+  scan2 = Scan.create(profile_id: user.profile.id, exhibit_id: exhibit2.id)
