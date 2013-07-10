0c6ad6f95dd249a892baaec8d69efd63b9dbed10
rares123 <cruceat.rares@yahoo.com>
2013-07-10 12:18:06 +0300

    Add validators for scans

--- app/models/scan.rb
+++ app/models/scan.rb
@@ -4,6 +4,8 @@ class Scan < ActiveRecord::Base
   belongs_to :exhibit
   belongs_to :profile
 
+  validates :exhibit_id, presence: true
+  validates :profile_id, presence: true
   def self.scanned?(ex_id, prof_id)
 		unless Scan.where(exhibit_id: ex_id, profile_id: prof_id).first
 			Scan.create(exhibit_id: ex_id, profile_id: prof_id)
