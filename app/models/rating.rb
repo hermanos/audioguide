e02b046c6272e8515a0dd40ece8e404d5a91dd6a
rares123 <cruceat.rares@yahoo.com>
2013-07-10 12:17:02 +0300

    Add validators for rating

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,3 +1,7 @@
 class Rating < ActiveRecord::Base
   attr_accessible :exhibit_id, :score, :user_id
+
+  validates :exhibit_id, presence: true
+  validates :score, presence: true
+  validates :user_id, presence: true
 end
