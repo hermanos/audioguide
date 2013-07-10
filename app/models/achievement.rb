cc400b2ac11f82ea9c68f786f6dcf3145bbcae28
rares123 <cruceat.rares@yahoo.com>
2013-07-10 11:32:35 +0300

    Add validators to achievements

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -2,4 +2,7 @@ class Achievement < ActiveRecord::Base
   attr_accessible :description, :image, :profile_id, :title
 
   has_and_belongs_to_many :profiles
+
+  validates :title, presence: true
+  validates :description, presence: true
 end
