c03449b365fe906eb2bf8d8b37fe1e8384146e38
rares123 <cruceat.rares@yahoo.com>
2013-07-11 16:21:39 +0300

    Add upload image for profile

--- db/migrate/20130702132952_create_profiles.rb
+++ db/migrate/20130702132952_create_profiles.rb
@@ -2,7 +2,6 @@ class CreateProfiles < ActiveRecord::Migration
   def change
     create_table :profiles do |t|
       t.integer :user_id
-      t.string :avatar
       t.string :name
       t.string :role
       t.integer :coins
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130711125405) do
+ActiveRecord::Schema.define(:version => 20130711131423) do
 
   create_table "achievements", :force => true do |t|
     t.string   "title"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(:version => 20130711125405) do
     t.integer  "coins"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "image"
   end
 
   create_table "ratings", :force => true do |t|
