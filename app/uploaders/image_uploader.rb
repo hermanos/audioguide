a7d3e9632e0ddee25e2f6b0f82115e21dc39cdb4
rares123 <cruceat.rares@yahoo.com>
2013-07-11 17:01:26 +0300

    Thumbs and put uploads to gitignore

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -35,7 +35,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   version :thumb do
      process :resize_to_fit => [50, 50]
   end
-
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
