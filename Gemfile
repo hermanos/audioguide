ea6765c24e39fedd61b3176c894877547b520388
davidlinx <david_suilea@yahoo.com>
2013-07-16 17:02:25 +0300

    Deploy on AppFog

--- Gemfile
+++ Gemfile
@@ -1,7 +1,6 @@
 source 'https://rubygems.org'
 
 
-
 gem 'rails', '3.2.13'
 
 # Bundle edge Rails instead:
@@ -39,6 +38,7 @@ group :test, :development do
 end
 
 gem "capybara"
+gem 'nokogiri', '1.3.3'
 
 # To use ActiveModel has_secure_password
 gem 'bcrypt-ruby', '~> 3.0.0'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-AudioguideMuseum::Application.config.session_store :cookie_store, key: '_audioguideMuseum_session'
+AudioguideMuseum::Application.config.session_store(:cookie_store, key: '_audioguideMuseum_session2')
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
