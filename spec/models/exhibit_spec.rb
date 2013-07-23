4236cf4f1fc8c5139897f64959334ea32ae6e992
davidlinx <david_suilea@yahoo.com>
2013-07-23 10:59:33 +0300

    Working of Rspec tests

---
+++ spec/models/exhibit_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe Exhibit do
+	xit "checks if the exhibit is private" do
+		
+	end
+
+end
--- spec/models/scan_spec.rb
+++ spec/models/scan_spec.rb
@@ -1,15 +1,17 @@
 require 'spec_helper'
 
 describe Scan do
-	it "check if the self.scan function works properly" do
-		user = User.create!(email: 'vasilica@mymail.com', password: '123456789')
-		muzeu = Museum.create!(name: 'Muzeul de Stiinte Naturale', lat: 45.75515, long: 24.116297, description: "Cel mai tare muzeu", manager_id: user, published: 0 )
-		exhibit = muzeu.exhibits.create!(title: 'Monalisa', description: 'Pictura', audio: nil, video: nil, floor: 0, lat: 40, long: 40, qr_code: 'whatevaaaaaaa', private: 0)
+	context "; if the user scans an exhibit," do
+		it " checks if a new scan is created if the exhibit was not scaned in the past" do
+			user = User.create!(email: 'vasilica@mymail.com', password: '123456789')
+			muzeu = Museum.create!(name: 'Muzeul de Stiinte Naturale', lat: 45.75515, long: 24.116297, description: "Cel mai tare muzeu", manager_id: user, published: 0 )
+			exhibit = muzeu.exhibits.create!(title: 'Monalisa', description: 'Pictura', audio: nil, video: nil, floor: 0, lat: 40, long: 40, qr_code: 'whatevaaaaaaa', private: 0)
 
-		visit museum_exhibit_url(museum, exhibit)
-		scan = Scan.find(ex_id: exhibit, prof_id: user.profile)
+			visit museum_exhibit_url(museum, exhibit)
+			scan = Scan.find(ex_id: exhibit, prof_id: user.profile)
 
-		expect(scan).no_to eq(nul)	
+			expect(scan).no_to eq(nul)	
+		end
 	end
 	
 end
