edc744c9987a0a6436d86ad01d6dea51c32eae8b
rares23 <cruceat.rares@yahoo.com>
2013-07-24 16:22:43 +0300

    Delete stylesheets

--- app/models/qr_code.rb
+++ app/models/qr_code.rb
@@ -1,13 +1,13 @@
 class QrCode < ActiveRecord::Base
   attr_accessible :qrcode, :status, :counts
 
-  def self.scan(qr,status)
+  def self.scan(qr, status)
   	qrcodes = QrCode.where(qrcode: qr)
   	if qrcodes.count == 0
   		QrCode.create!(qrcode: qr, status: status)
   	else
   		qrcode = qrcodes.first
-  		qrcode.update_attribute(:counts, qrcode.counts+1)
+  		qrcode.update_attribute(:counts, qrcode.counts + 1)
   	end
   end
 end
