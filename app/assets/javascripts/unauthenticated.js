2efb2244c23deccaf528634713f17a127ac4cd9b
corco <iuly_corco@yahoo.com>
2013-07-30 16:15:43 +0300

    try a download bar

--- app/assets/javascripts/unauthenticated.js
+++ app/assets/javascripts/unauthenticated.js
@@ -3,15 +3,10 @@ $(document).ready(function() {
 	$('a#hide-button').click(function(e) {
 		e.preventDefault();
 		e.stopPropagation();
-		$('h1').hide();
+		$('p#download-text').hide();
 		$(this).hide();
 		$(this).parent().animate({height:"52px"});	
 		$(this).parent().css('padding', '0px');
-		$('a#download').css({'padding': '10px',
-													'float': 'top', 
-													'margin-top': '-10px', 
-													'width': '90%'});
-		$('a#download').text('Get the Audio-Guide feature now !')
 	});
 
 });
