37880a85800664425fa5698aba179e8fc03fbb3e
corco <iuly_corco@yahoo.com>
2013-07-30 19:09:40 +0300

    smth javascript and stylesheet

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,6 +1,4 @@
 $(document).ready(function () {
-
-  // $("body").backstretch("/images/background.jpg");
   $('html.home div.background-up').backstretch("/assets/background.jpg");
- 	$('html.home-user body').backstretch("/assets/background.jpg");  
+ 	$('html.home-user body').backstretch("/assets/background.jpg");
 });
